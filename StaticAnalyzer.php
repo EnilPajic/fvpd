d874b6d03979c3153ddfe50c21b799f010fbc9fa
vljubovic <vljubovic@smartnet.ba>
2018-04-20 10:21:11 +0200

    Don't count ~ as neg_op because 1) it's used in C++ destructors and 2) we count it as rare op

--- StaticAnalyzer.php
+++ StaticAnalyzer.php
@@ -14,7 +14,7 @@ class StaticAnalyzer
     private static $regex_modulaincdec = '/(?:%=|%|\+\+|\-\-)/i';
     private static $regex_iili = '/(&&|\|\||\band\b|\bor\b)/i';
     private static $regex_istirazliciti = '/(==|\!=|not_eq)/i';
-    private static $regex_notneg = '/(?:~|![^=]|\bnot\b|\bcompl\b)/i';
+    private static $regex_notneg = '/(?:![^=]|\bnot\b|\bcompl\b)/i';
     private static $regex_compare = '/(?:>=|[^>]>[^>]|<=|[^<]<[^<])/i';
     private static $regex_rijetkiop = '/(?:&=|\|=|(?<!\|)\|(?!\|)|\^=|\^|&=|\.\*|\-\>|\*\*\*\*|\*\*\*|\.\.\.|~)/i';
     private static $regex_include = '/#\h*include.*/i';
