e8d23de6b9b93f618fb9ef7ac0b968b7513f9846
vljubovic <vljubovic@smartnet.ba>
2018-04-20 09:48:19 +0200

    Count C++ methods with namespaces and inspectors as functions

--- StaticAnalyzer.php
+++ StaticAnalyzer.php
@@ -7,7 +7,7 @@ class StaticAnalyzer
     private static $regex_word = '/(\w+)/i';
     private static $regex_loop = '/\b(for|while|goto)\b/i';
     private static $regex_branching = '/\b(if|else\s+if|else|switch|case|default)\b/i';
-    private static $regex_string = '/"(.*?[^\\\\])"/i';
+    private static $regex_string = '/"(.*?[^\\\\])"/i'; # GREŠKA ne broji prazan string ""
     private static $regex_charliteral = '/\'(.*?[^\\\\])\'/i';
     private static $regex_plusminus = '/(?:\+=|[^\+]\+[^\+]|\-=|[^\-]\-[^\-])/i';
     private static $regex_putadijeli = '/(\*=|\*|\/=|\/)/i'; # GREŠKA broji deklaracije pointera
@@ -20,7 +20,7 @@ class StaticAnalyzer
     private static $regex_include = '/#\h*include\h*/i';
     private static $regex_trycatch = '/\btry\b/i';
     private static $regex_tipovi = '/(?:(?:unsigned\s+|signed\s+)?(?:\blong\s+long\s+int\b|\blong\s+long\b|\blong\b|\bchar\b|\bshort\s+int\b|\bshort\b|\bint\b|\bbool\b))|\blong double\b|\bdouble\b|\bfloat\b|\bFILE\b|\bunsigned\b|\bsigned\b|\bvoid\b/';
-    private static $regex_fun = '/([a-zA-Z0-9_>]+)(?<!else)\s*\*{0,5}\s+[a-zA-Z0-9_>]+\s*?\(.*\)\s*{/i';
+    private static $regex_fun = '/([a-zA-Z0-9_>:]+)(?<!else)\s*\*{0,5}\s+[a-zA-Z0-9_>:]+\s*?\(.*\)\s*(const)?\s*{/i';
     private static $regex_nizovi = '/[a-zA-Z0-9_>]+(?<!else)(?:\**\s+\**\s*|\*+|,)[a-zA-Z0-9_>]+\s*\[\s*(?:\d{0,12}|.*?)\s*\]/i';
     private static $regex_svibrojevi = '/\d{1,50}/i';
     private static $regex_brojevi = '/\b\d+\b/i';
